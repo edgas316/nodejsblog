ba3dd1b7b02f95b7a5763f212b60ff473b63674f
gaspaed <Edvin.Gasparian@verizonwireless.com>
2015-11-30 16:54:14 -0800

    end of day

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator')
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodejsblog')
+var multer = require('multer')
+var upload = multer({ dest: './public/images/uploads'})
+var flash = require('connect-flash')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.locals.moment = require('moment')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +29,45 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Express Session
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave:true
+}))
+
+// Express validator
+app.use(expressValidator({
+    errorFormator: function(param,msg,value){
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root
+        while(namespace.length){
+            formParam += '['+namespace.shift()+']'
+        }
+        return{
+            param:formParam,
+            msg:msg,
+            value:value
+        }
+    }
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect flash
+app.use(flash())
+app.use(function(req,res,next){
+    res.locals.messages = require('express-messages')(req,res)
+    next()
+})
+
+// Make db accessable to our router
+app.use(function(req,res,next){
+    req.db = db
+    next()
+})
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb')
+var db = require('monk')('localhost/nodejsblog')
 
-/* GET home page. */
+// Home Page Blog Post
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    var db = req.db,
+        posts = db.get('posts')
+    posts.find({},{},function(err, posts){
+        res.render('index',{
+            "posts":posts
+        })
+    })
+})
 
 module.exports = router;
